bbfe59c151fcceb924d622d6e107e12777be6c97
liqing1620 <2501540001@qq.com>
2019-04-04 17:20:07 +0800

    updata

---
+++ AngularJS-Demo/static/js/data.js
@@ -0,0 +1,11 @@
+var t_img = "./static/images/1168383.png";
+
+var navs = [{title: "AngularJS基础知识"}, {title: "第二个标题"}]
+
+
+var con = [{title:"AngularJS基础知识",txt:"AngularJS 是一个 JavaScript 框架。它可通过 script 标签添加到 HTML 页面。\n" +
+        "\n" +
+        "AngularJS 通过 指令 扩展了 HTML，且通过 表达式 绑定数据到 HTML。AngularJS 是一个 JavaScript 框架\n" +
+        "AngularJS 是一个 JavaScript 框架。它是一个以 JavaScript 编写的库。\n" +
+        "\n" +
+        "AngularJS 是以一个 JavaScript 文件形式发布的，可通过 script 标签添加到网页中："}]
