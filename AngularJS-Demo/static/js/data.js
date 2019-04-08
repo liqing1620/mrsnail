f6e1ee9050a90e679ebb0c0a572d5eff5e7ce57e
liqing1620 <2501540001@qq.com>
2019-04-08 11:31:47 +0800

    update

--- AngularJS-Demo/static/js/data.js
+++ AngularJS-Demo/static/js/data.js
@@ -1,16 +1,15 @@
 var t_img = "./static/images/1168383.png";
 
-var navs = [{title: "AngularJS基础知识"}, {title: "AngularJS基础知识"}]
+var navs = [
+    {url: '/one', template: '/templates/index_one.html', title: "AngularJS基础知识"},
+    {url: '/two', template: '/templates/index_two.html', title: "AngularJS基础知识"}]
 
 
-var con = [{title:"AngularJS基础知识",txt:"AngularJS 是一个 JavaScript 框架。它可通过 script 标签添加到 HTML 页面。\n" +
-        "\n" +
+var con = [{
+    title: "AngularJS基础知识", txt: "AngularJS 是一个 JavaScript 框架。它可通过 script 标签添加到 HTML 页面。" +
         "AngularJS 通过 指令 扩展了 HTML，且通过 表达式 绑定数据到 HTML。AngularJS 是一个 JavaScript 框架\n" +
         "AngularJS 是一个 JavaScript 框架。它是一个以 JavaScript 编写的库。\n" +
         "\n" +
-        "AngularJS 是以一个 JavaScript 文件形式发布的，可通过 script 标签添加到网页中："},{title:"AngularJS基础知识",txt:"AngularJS 是一个 JavaScript 框架。它可通过 script 标签添加到 HTML 页面。\n" +
-        "\n" +
-        "AngularJS 通过 指令 扩展了 HTML，且通过 表达式 绑定数据到 HTML。AngularJS 是一个 JavaScript 框架\n" +
-        "AngularJS 是一个 JavaScript 框架。它是一个以 JavaScript 编写的库。\n" +
-        "\n" +
-        "AngularJS 是以一个 JavaScript 文件形式发布的，可通过 script 标签添加到网页中："}]
+        "AngularJS 是以一个 JavaScript 文件形式发布的，可通过 script 标签添加到网页中："
+},
+    {title: "AngularJS基础知识", txt: "AngularJS 是一个 JavaScript 框架。它可通过 script 标签添加到 HTML 页面。"}]
